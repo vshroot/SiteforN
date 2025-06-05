5f65868decb624bb31b0d214aeaf5d4826fe359e
vshroot <vladislavshapran@gmail.com>
2025-06-05 13:28:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function checkPassword() {
+  const correctPassword = "1234"; // Задай свой пароль здесь
+  const entered = document.getElementById("password").value;
+
+  if (entered === correctPassword) {
+    window.location.href = "player.html";
+  } else {
+    document.getElementById("error").textContent = "Неверный пароль!";
+  }
+}
